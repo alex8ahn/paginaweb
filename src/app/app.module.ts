df0fc6bbad4f626c2f8f481c07db62dd490dd835
alex8ahn <alex8a1991@gmail.com>
2022-07-11 14:46:32 -0600

    template listo sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HerocarouselComponent } from './pages/herocarousel/herocarousel.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HerocarouselComponent,
+    NoticiasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/herocarousel/herocarousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-herocarousel',
+  templateUrl: './herocarousel.component.html',
+  styleUrls: ['./herocarousel.component.css']
+})
+export class HerocarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
